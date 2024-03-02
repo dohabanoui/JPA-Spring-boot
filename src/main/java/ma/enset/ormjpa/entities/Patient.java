b9f799ece1e2fe4488e6935fcd92047042fc26d1
dohabanoui <dohabanoui1@gmail.com>
2024-03-02 01:48:51 +0100

    first commit

---
+++ src/main/java/ma/enset/ormjpa/entities/Patient.java
@@ -0,0 +1,24 @@
+package ma.enset.ormjpa.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Patient {
+    // une entity doit avoir un id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private Date dateNaissance;
+    private boolean malade;
+    private int score;
+
+}
